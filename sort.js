36c08f977bf4ba8f375318e2a8d898e62e91b052
hanabi <hanabi@test.co.jp>
2020-07-08 18:41:16 +0900

    指摘箇所の修正

--- sort.js
+++ sort.js
@@ -1,12 +1,14 @@
 var sortNumber = function (number) {
    number.sort(function (a, b) {
-       if (a == b) {
+       if (a === b) {
            return 0;
        }
+       // 問題なし	   
        return a < b ? -1 : 1;
    });
 };
 
 var number = [19, 3, 81, 1, 24, 21];
 sortNumber(number);
+// 良いよ
 console.log(number);
